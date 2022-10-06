4cc9cb950ad777d2163cbe4b4894d0ac6abcea81
ripeagle <ripeaglee@hotmail.com>
2022-10-06 13:48:10 +0200

    .

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ dt = datetime.now()
 x = dt.weekday()
 x = x + 1
 
+#Json code from https://www.geeksforgeeks.org/json-load-in-python/
+f = open('klasser.json',)
+   
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+   
+# Iterating through the json
+# list
+for i in data['overwriteOtherData']['data']['classes']:
+    print(i)
+   
+# Closing file
+f.close()
+
+
 # Get the data from the Gettime's database 'https://gettime.ga/API/JSON
 params = {
     'school': '0',
@@ -20,5 +36,29 @@ params = {
 
 response = requests.get('https://gettime.ga/API/JSON', params=params)
 data = json.loads(response.text)
-for lesson in data["data"]["data"]["lessonInfo"]:
-    print(lesson)
+
+a = []
+
+try:
+    for x in data['data']['data']['lessonInfo']:
+        temp = f"{x['timeStart']} -- {x['texts'][0]}, börjar kl {x['timeStart']} och slutar kl {x['timeEnd']}"
+        try:
+            temp += f" i sal {x['texts'][2]}"
+        except:
+            pass
+        a.append(temp)
+    a.sort()
+
+    a = [i.split(' -- ')[1] for i in a]
+
+    for x in a:
+        print(x+'\n')
+except TypeError:
+    print("No class found with such name")
+
+
+params = {
+    'school': '0',
+    'id': '20EL1',
+    'day': x,
+}
