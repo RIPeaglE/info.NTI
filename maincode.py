136a9a584cd7862001c332eab6431963cf0a23fc
RIPeaglE <ripeaglee@hotmail.com>
2022-11-23 20:21:18 +0100

    .

--- maincode.py
+++ maincode.py
@@ -72,7 +72,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('frontend.html'), time()
+    return render_template('test2.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
